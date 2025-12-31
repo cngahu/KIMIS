1354b233139d0b75e15ee51d372e42aba626b67c
cngahu <papacosi@gmail.com>
2025-12-31 11:07:55 +0300

    add gender

---
+++ database/migrations/2025_12_31_082756_add_gender_to_master_data_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('masterdata', function (Blueprint $table) {
+            $table->char('gender', 1)->nullable()->after('id');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('masterdata', function (Blueprint $table) {
+            $table->dropColumn('gender');
+        });
+    }
+
+};
